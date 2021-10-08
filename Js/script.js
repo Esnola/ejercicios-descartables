d283b2536c2e63c9fe17cec8edf736cfc7e8a1e4
Esnola <esnola@gmail.com>
2021-10-08 20:11:53 +0200

    Empezamos con JS'

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+let numeros=[1, 2, 3];
+
+let uno = numeros[0],
+    dos = numeros[1],
+    tres = numeros[2];
+console.log(uno, dos, tres);
+
+const [one, two, three] = numeros;
+console.log(one, two, three)
