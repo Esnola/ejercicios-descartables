01ab370582c1ccf9175c608c3f26eafa6e2ae564
Esnola <esnola@gmail.com>
2021-10-08 14:49:56 +0200

    Imágenes añadidas

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+const miClick = document.addEventListener('click', escucha);
+const hache1 = document.querySelector('h1');
+const primerP = document.querySelector('.primero');
+function escucha() {
+   /*  console.log(primerP )
+    hache1.classList.add('rojo'); */
+
+} 
